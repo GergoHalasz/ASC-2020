f7ae527c22323f24018116c45eed46d9d8f0d5c1
HalaszGergo123 <halasz.gergo2001@gmail.com>
2020-10-15 19:43:57 +0300

    tema rezolvat

--- Moore/Program.cs
+++ Moore/Program.cs
@@ -12,7 +12,16 @@ namespace Moore
         {
 
             // rezolva problema
+            
+            int n;
+            Console.WriteLine("Scrieti numarul n:");
+            n = int.Parse(Console.ReadLine());
+            int luni = Convert.ToInt32(Math.Floor((Math.Log(n, 2)) * 18));
+            int ani = luni / 12;
+            luni = luni % 12;
 
+     
+            Console.WriteLine($"Dupa {ani} ani si {luni} luni vom avea o puterea de calcul de {n} ori mai mare, la acelasi pret.");
 
         }
     }
