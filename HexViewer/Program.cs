187dd8f4408716d6cd2311ccfec8bfaad25797fe
HalaszGergo123 <halasz.gergo2001@gmail.com>
2020-11-23 18:14:33 +0200

    hexviewer done

--- HexViewer/Program.cs
+++ HexViewer/Program.cs
@@ -12,17 +12,63 @@ namespace HexViewer
     {
         static void Main(string[] args)
         {
-            string path = @"A:\Untitled.txt";
+            bool done = false;
 
-            if(File.Exists(path))
+            while(!done)
             {
-                File.Delete(path);
+
+                try
+                {
+                    
+                    Console.WriteLine("Introduceti calea fisierului pentru a-l vizualiza prin HexViewer:\n");
+                    string path = Console.ReadLine();
+                    Console.WriteLine();
+
+                    Console.WriteLine("Introduceti cati octeti vreti sa fie pe o singura linie:\n");
+                    int nrOcteti = int.Parse(Console.ReadLine());
+                    Console.WriteLine();
+
+
+                    FileStream file = new FileStream(path,FileMode.Open);
+
+                   
+                    char[] caractereDeEliminat = new char[] { ' ', '"' };
+                    path = path.Trim(caractereDeEliminat);
+                    byte[] byteBlock = new byte[nrOcteti];
+                    int idx = 0;
+
+                    while(file.Read(byteBlock, 0, nrOcteti)>0)
+                    {
+                        string hex = BitConverter.ToString(byteBlock);
+                        hex = hex.Replace("-", " ");
+                        string text = "";
+                        for (int i = 0; i < byteBlock.Length; i++)
+                       text += byteBlock[i] < ' ' ? "." : ((char)byteBlock[i]).ToString();
+
+                        Console.WriteLine($" {idx:X8} : {hex.PadRight(nrOcteti * 3 - 1)}  | {text}");
+                        idx += nrOcteti;
+
+
+
+
+
+                    }
+
+                    file.Close();
+                    done = true; 
+
+                }
+
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+
+                }
             }
-            using (FileStream fs = File.Create(path))
-            {
-               
-               
-            }
+          
+           
+           
+
 
         }
     }
